2db8299bea0d29cc102e880098ffcbd28a468c94
Ong Wee Keong <ongweekeong@gmail.com>
2019-10-17 21:58:21 +0800

    Working version compatible with Python 2.6

--- sudoku_A2_29.py
+++ sudoku_A2_29.py
@@ -1,6 +1,6 @@
 import sys
 import copy
-import queue
+import Queue
 
 rows = "ABCDEFGHI"
 cols = "123456789"
@@ -9,10 +9,36 @@ def add(x, y):
 
 	return [xi+yi for xi in x for yi in y]
 
+def get_row(var):
+	return {
+		'A': 0,
+		'B': 1,
+		'C': 2,
+		'D': 3,
+		'E': 4,
+		'F': 5,
+		'G': 6,
+		'H': 7,
+		'I': 8
+		}[var[0]]
+
+def get_col(var):
+	return {
+		'1': 0,
+		'2': 1,
+		'3': 2,
+		'4': 3,
+		'5': 4,
+		'6': 5,
+		'7': 6,
+		'8': 7,
+		'9': 8
+		}[var[1]]		
+
 squares = [row+col for row in rows for col in cols] # Names the squares from A1...to...I9
 groups = ([add(row, cols) for row in rows] +
 		 [add(rows, col) for col in cols] +
-		 [add(r, c) for r in ("ABC", "DEF", "GHI") for c in ("123", "456", "789")]) # All 27 groups of 9 variables for the 27 AllDiff constraints
+		 [add(row, col) for row in ("ABC", "DEF", "GHI") for col in ("123", "456", "789")]) # All 27 groups of 9 variables for the 27 AllDiff constraints
 square_in_groups = dict((x, [y for y in groups if x in y]) for x in squares) # Dictionary (x:y) : x is the variable, y is a list of the 3 groups that the variable belongs to   	
 
 class Sudoku(object):
@@ -20,23 +46,21 @@ class Sudoku(object):
 		# you may add more attributes if you need
 		self.puzzle = puzzle # self.puzzle is a list of lists
 		self.variables = squares
-		#self.values = dict.fromkeys(squares)
 		self.neighbours = self.get_neighbours()
 		self.domains = self.get_domains()
 		self.constraints = set((x, y) for x in squares for y in self.neighbours[x])
-		self.ans = copy.deepcopy(puzzle) # self.ans is a list of lists
+		self.ans = copy.copy(puzzle) # self.ans is a list of lists
 
 	def solve(self):
 		#TODO: Your code here
-		self.AC3()
-		assignment = self.get_assignment()
-		solved_puzzle = self.backtracking_search(assignment)
-		self.get_solution(solved_puzzle)
+		self.AC3() # Preprocessing
+		solved_puzzle = self.backtracking_search({})
 
 		# don't print anything here. just return the answer
 		# self.ans is a list of lists
 		return self.ans
 
+	# Returns a dictionary of a permanent list of neighbours for each variable
 	def get_neighbours(self):
 		neighbours = dict((x, set()) for x in squares)
 
@@ -48,6 +72,7 @@ class Sudoku(object):
 
 		return neighbours	
 
+	# Computes the domain of each variable from the initial state of the puzzle
 	def get_domains(self):
 		domains = dict((x, []) for x in squares)
 
@@ -56,7 +81,6 @@ class Sudoku(object):
 		for variable in squares:
 			if self.puzzle[i][j] != 0:
 				domains[variable].append(puzzle[i][j])
-				#self.values[variable] = puzzle[i][j]
 			else:
 				domains[variable].extend([1,2,3,4,5,6,7,8,9])
 
@@ -67,18 +91,9 @@ class Sudoku(object):
 
 		return domains	
 
-	# Get the starting assignments from the starting puzzle
-	def get_assignment(self):	
-		assignment = {}
-
-		for variable in self.domains:
-			if len(self.domains[variable]) == 1:
-				assignment[variable] = self.domains[variable][0]
-
-		return assignment
-
+	#____________________________________________________________	
 	def AC3(self):
-		q = queue.Queue()
+		q = Queue.Queue()
 
 		for constraint in self.constraints:
 			q.put(constraint)
@@ -119,30 +134,32 @@ class Sudoku(object):
 		return self.backtrack(assignment)
 
 	def backtrack(self, assignment):
-
 		if self.is_complete(assignment):
 			return assignment
- 
+ 		
+ 		# Keeps the current copy of domain to be used for a backtrack in the future
 		domain = copy.deepcopy(self.domains)    
 
 		var = self.select_variable(assignment)
 
 		if var in self.domains:	
-			for assigning_value in self.domains[var]:
-				if self.isConsistent(var, assigning_value, assignment):
-					assignment[var] = assigning_value
-					check = self.forward_checking(domain, var, assigning_value, assignment) 
+			for value in self.domains[var]:
+				if self.isConsistent(var, value, assignment):
+					assignment[var] = value
+					check = self.forward_checking(var, value, assignment) 
 
-					if check != {"FAILURE"}:
+					if check:
 						result = self.backtrack(assignment) # Calls backtrack again for the next variable assignment
-						if result != {"FAILURE"}:
+						if len(result) != 0: # != {"FAILURE"}:
+							self.puzzle[get_row(var)][get_col(var)] = value
 							return result
 
 					del assignment[var]
-					self.domains.update(domain)	
+					self.domains.update(domain)	# Wrong assignment. Returning domains to previous values before the current assignment
 
-		return {"FAILURE" : "FAILURE"}            
+		return {}
 
+	# Checks if we have a complete assignment that is consistent
 	def is_complete(self, assignment):
 
 		if len(assignment) == 81:
@@ -152,78 +169,42 @@ class Sudoku(object):
 
 	# Checks if any variable xj in the same group as this variable xi has already been assigned value
 	def isConsistent(self, var, value, assignment):
-
 		for neighbour in self.neighbours[var]:
 			if neighbour in assignment and assignment[neighbour] == value:
 				return False
 
 		return True        
 
-	# Uses minimum remaining values heuristic
+	# Uses minimum remaining values heuristic to select the next variable to be assigned
 	def select_variable(self, assignment):
-		min_variables = dict({x, len(self.domains[x])} for x in squares if x not in assignment)
-
+		min_variables = dict((x, len(self.domains[x])) for x in squares if x not in assignment)
 		min_var = min(min_variables, key=min_variables.get)
 		
 		return min_var		
 
-	# Maintaining arc consistency
-	def forward_checking(self, domain, var, value, assignment):
-
-		q = queue.Queue()
-
+	# Does forward checking to see if the current assignment (of value to var) results in failure by checking if there are still values to be assigned
+	# to the neighbours of var, and to the neighbours of the neighbours of var (somewhat between Forward Checking and Maintaining Arc Consistency) 
+	def forward_checking(self, var, value, assignment):
 		for neighbour in self.neighbours[var]:
-			if neighbour not in assignment and value in domain[neighbour]:
-				domain[neighbour].remove(value) # Might have problems with this
+			if neighbour not in assignment and value in self.domains[neighbour]:
+				self.domains[neighbour].remove(value) 
 
-			if not domain[neighbour]: # when domain neighbour becomes empty
-				return {"FAILURE"}
+			if not self.domains[neighbour]: # when domain of neighbour becomes empty
+				return False
+				#return {"FAILURE"}
 
-			if len(domain[neighbour]) == 1:
-				neighbour_value = domain[neighbour][0]
-				q.put(neighbour)
-
+			if len(self.domains[neighbour]) == 1:
+				neighbour_value = self.domains[neighbour][0]
+			
 				for check_neighbour in self.neighbours[neighbour]:
-					if check_neighbour not in assignment and neighbour_value in domain[check_neighbour]:
-						domain[check_neighbour].remove(neighbour_value)
+					if check_neighbour not in assignment and neighbour_value in self.domains[check_neighbour]:
+						self.domains[check_neighbour].remove(neighbour_value)
 
-					if not domain[check_neighbour]:
-						return {"FAILURE"}	
-				
-		"""while not q.empty():
-			forward_check = q.get()
-			check_var = forward_check[0]
-			check_value = domain[forward_check]
+					if not self.domains[check_neighbour]:
+						return False
+					#	return {"FAILURE"}	
 
-			for neighbour in self.neighbours[check]:
-				if neighbour not in assignment and value in domain[neighbour]:
-					domain[neighbour].remove(value)
-
-				if not domain[neighbour]: # when domain neighbour becomes empty
-					return {"FAILURE"}
-
-				if len(domain[neighbour]) == 1:
-					q.put(neighbour)
-
-					for check_neighbour in self.neighbours[neighbour]:
-						q.put((neighbour))"""
-
-		return domain
-
-	# copy ans from backtrack into self.puzzle
-
-	def get_solution(self, solved_puzzle):
-
-		i, j = 0, 0
-		for variable in squares:
-			self.puzzle[i][j] = solved_puzzle[variable]
-
-			j = j + 1   
-			if j == 9:
-				i = i + 1
-				j = 0
-
-		return solved_puzzle
+		return True
 
 	# you may add more classes/functions if you think is useful
 	# However, ensure all the classes/functions are in this file ONLY
